0410075ef78bdf869f19c3570a9051c720c78e0b
lfowks <la.fowks@gmail.com>
2023-09-08 21:03:24 -0600

    Add project files.

---
+++ EventSource/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using System;
+using EventSource;
+using Newtonsoft.Json;
+using Quartz.Impl;
+using Quartz;
+using QuartzSample;
+
+
+// 1. Create a scheduler Factory
+ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+
+// 2. Get and start a scheduler
+IScheduler scheduler = await schedulerFactory.GetScheduler();
+await scheduler.Start();
+
+// 3. Create a job
+IJobDetail job = JobBuilder.Create<NumberGeneratorJob>()
+        .WithIdentity("number generator job", "number generator group")
+        .Build();
+
+// 4. Create a trigger
+ITrigger trigger = TriggerBuilder.Create()
+    .WithIdentity("number generator trigger", "number generator group")
+    .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever())
+    .Build();
+
+// 5. Schedule the job using the job and trigger 
+await scheduler.ScheduleJob(job, trigger);
+
+Console.ReadLine();
---
+++ EventSource/QuartzSample.cs
@@ -0,0 +1,48 @@
+﻿using EventSource;
+using Newtonsoft.Json;
+using Quartz;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzSample
+{
+    public class NumberGeneratorJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            using var client = new HttpClient();
+
+            var user = new User();
+
+            user.Id = RandomNumber(1, 1000);
+            user.Name = "Name1";
+            user.Email = "Email1";
+            user.Password = "";
+
+
+            var json = JsonConvert.SerializeObject(user);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "https://localhost:7185/api/notification/";
+
+            try
+            {
+                var response = await client.PostAsync(url, data);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            await Task.CompletedTask;
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            var random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
